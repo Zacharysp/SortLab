490e07a4b4797a9baaf9fd902f7b6e5228923890
Zacharysp <sp151018@gmail.com>
2016-10-30 14:26:12 +0800

    Init project

---
+++ SortLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SortLab
+//
+//  Created by Zachary on 10/30/16.
+//  Copyright © 2016 Zachary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
